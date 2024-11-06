8af41a41d3957e7ec1e9550f662919be481ea76d
Elam Cano <elamcano16@gmail.com>
2024-11-06 13:50:24 -0300

    add mongodb atlas config

--- server/util/cronJob.ts
+++ server/util/cronJob.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { people, films, planets, starships } from "../controllers/index";
 
 //Se ejecuta todos los dias a las 00:00 am
-cron.schedule("0 0  * * *", async () => {
+cron.schedule("48 13  * * *", async () => {
   console.log("Ejecutando sincronización de datos...");
   await people();
   await films();
